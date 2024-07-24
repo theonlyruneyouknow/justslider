8f0a455c521c2dc208f894e8c0c183aefd3f57ed
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-07-24 14:24:38 -0700

    All controlls are in it place and works as they should at this point.

--- dualplusslider/togglebuttons.js
+++ dualplusslider/togglebuttons.js
@@ -6,18 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             if (button.textContent === "Remove") {
                 button.textContent = "Restore";
+                button.style.backgroundColor = '#007bff'; // Change to blue
             } else {
                 button.textContent = "Remove";
+                button.style.backgroundColor = '#28a745'; // Change to green
             }
         });
     });
 
     showHideButtons.forEach(button => {
+        button.textContent = "Show Category"; // Initial state
+        button.style.backgroundColor = '#28a745'; // Initial green color
         button.addEventListener("click", function() {
-            if (button.textContent === "Hide Category") {
-                button.textContent = "Show Category";
-            } else {
+            if (button.textContent === "Show Category") {
                 button.textContent = "Hide Category";
+                button.style.backgroundColor = '#007bff'; // Change to blue
+            } else {
+                button.textContent = "Show Category";
+                button.style.backgroundColor = '#28a745'; // Change to green
             }
         });
     });
